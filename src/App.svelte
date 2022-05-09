51a2e5be1aeb776f71eb5ab307b52a1029b60570
airmanxtw <airmanx@stust.edu.tw>
2022-05-10 00:52:26 +0800

    first

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Button from '@smui/button'
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<Button>
+		大家好2
+	</Button>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
